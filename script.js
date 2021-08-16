6d4cc6432136f7f9e25d3991af691cb433061de2
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-16 12:11:14 +0300

    add form

--- script.js
+++ script.js
@@ -1,105 +1,143 @@
+const inc =
+  (init = 0) =>
+  () =>
+    ++init;
+const genId = inc();
+
+const createTask = (data) => {
+  return {
+    id: genId(),
+    title: data.title,
+    done: false,
+    due_date: data.due_date || "",
+    description: data.description || "",
+  };
+};
+const options = { year: "numeric", month: "numeric", day: "numeric" };
+const date1 = new Date("8/16/2021");
+const date2 = new Date("8/15/2021");
+const date3 = new Date("8/17/2021");
+const date4 = new Date("8/16/2021");
+
 let tasks = [
-    {
-        id: 1,
-        title: "first task",
-        done: false,
-        due_date: "8/16/2021, 14:50",
-        description: "first description"
-    },
-    {
-        id: 2,
-        title: "second task",
-        done: true,
-        due_date: "8/15/2021, 14:00"
-    },
-    {
-        id: 3,
-        title: "third task",
-        done: false,
-        due_date: "8/17/2021, 00:00",
-        description: "third description"
-    },
-    {
-        id: 4,
-        title: "four task",
-        done: false,
-        due_date: "8/16/2021, 8:00",
-    },
+  {
+    id: genId(),
+    title: "first task",
+    done: false,
+    due_date: date1,
+    description: "first description",
+  },
+  {
+    id: genId(),
+    title: "second task",
+    done: true,
+    due_date: date2,
+  },
+  {
+    id: genId(),
+    title: "third task",
+    done: false,
+    due_date: date3,
+    description: "third description",
+  },
+  {
+    id: genId(),
+    title: "four task",
+    done: false,
+    due_date: date4,
+  },
 ];
 
 const check = (id) => {
-    let task = tasks.find(task => task.id == id);
-    task.done = !task.done;
-    document.getElementById(task.id).remove();
-    createDiv(task)
-} 
+  let task = tasks.find((task) => task.id == id);
+  task.done = !task.done;
+  document.getElementById(task.id).remove();
+  createDiv(task);
+};
 
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
 const createDiv = (task) => {
-    let div = document.createElement('div');
-    div.className = "task-style";
-    div.setAttribute("id", task.id);
-    div.innerHTML = createTag(task, checkDate(task.due_date));
-    task.done ? container_done.append(div) : container.append(div); 
-}
+  let div = document.createElement("div");
+  div.className = "task-style";
+  div.setAttribute("id", task.id);
+  div.innerHTML = createTag(task, checkDate(task.due_date));
+  task.done ? container_done.append(div) : container.append(div);
+};
 
 const createTag = (task, dateTag) => {
-    let taskTag;
-    if (task.done) {
-         taskTag = `<input type="checkbox" checked onchange="check(this.parentNode.id)"> 
-         <h3 style="text-decoration: line-through; color: darkgray;">${task.id + '.' + task.title}</h3> 
-         <p>${task.due_date || '' }</p>
+  let taskTag;
+  if (task.done) {
+    taskTag = `<input type="checkbox" checked onchange="check(this.parentNode.id)"> 
+         <h3 style="text-decoration: line-through; color: darkgray;">${
+           task.id + "." + task.title
+         }</h3> 
+         <p>${task.due_date.toLocaleDateString("en-US", options) || ""}</p>
          <p class="delete-task" onclick="deleteTask(this.parentNode.id)">✖</p>`;
-    }
-    else {
-        taskTag = `<input type="checkbox" onchange="check(this.parentNode.id)"> 
-        <h3> ${task.id + '.' + task.title}</h3> 
+  } else {
+    taskTag = `<input type="checkbox" onchange="check(this.parentNode.id)"> 
+        <h3> ${task.id + "." + task.title}</h3> 
         ${dateTag}
-        <p class="delete-task" onclick="deleteTask(this.parentNode.id)">✖</p>`
-    }
+        <p class="delete-task" onclick="deleteTask(this.parentNode.id)">✖</p>`;
+  }
 
-    taskTag += `<br>
-    <p class="description">${task.description || ''}</p>`;
+  taskTag += `<br>
+    <p class="description">${task.description || ""}</p>`;
 
-    return taskTag;
-}
+  return taskTag;
+};
 
 const deleteTask = (id) => {
-    document.getElementById(id).remove();
-    tasks.splice(id, 1);
-}
+  document.getElementById(id).remove();
+  tasks.splice(id, 1);
+};
 
 const checkDate = (date) => {
-    let dateTag;
-    if (new Date() >= new Date(date)) {
-        dateTag = `<p class="overdue-task">${date}</p>`;
-    }
-    else if (new Date() < new Date(date)) {
-        dateTag = `<p>${date}</p>`;
-    }
-    else {
-        dateTag = '';
-    }
-    return dateTag;
-}
+  let dateTag;
+  if (new Date() > date) {
+    dateTag = `<p class="overdue-task">${date.toLocaleDateString(
+      "en-US",
+      options
+    )}</p>`;
+  } else if (new Date() <= date) {
+    dateTag = `<p>${date.toLocaleDateString("en-US", options)}</p>`;
+  } else {
+    dateTag = "";
+  }
+  return dateTag;
+};
 
 let isOn = true;
 
 const collapseTab = () => {
-    const container = document.getElementById('container_done');
-    const doneTaskH = document.getElementById('done_task');
-    if (isOn) {
-        doneTaskH.innerHTML = 'Done task+';
-        container.style.display = 'none';
-    }
-    else {
-        doneTaskH.innerHTML = 'Done task-';
-        container.style.display = '';
-    }
-    isOn = !isOn;
-}
+  const container = document.getElementById("container_done");
+  const doneTaskH = document.getElementById("done_task");
+  if (isOn) {
+    doneTaskH.innerHTML = "Done task+";
+    container.style.display = "none";
+  } else {
+    doneTaskH.innerHTML = "Done task-";
+    container.style.display = "";
+  }
+  isOn = !isOn;
+};
 
-tasks.forEach(task => {
+tasks.forEach((task) => {
+  createDiv(task);
+});
+
+const form = document.getElementById("task_form");
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  const newForm = new FormData(form);
+  const data = Object.fromEntries(newForm.entries());
+  if (data.title) {
+    data.due_date = new Date(data.due_date);
+    const task = createTask(data);
+    tasks.push(task);
     createDiv(task);
-})
+    form.reset();
+  } else {
+    alert("Enter the title");
+  }
+});
